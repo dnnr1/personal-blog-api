48eebd360f0907107663a39c8ee2c69dd1977e01
Daniel Roberto <sdanirobert@outlook.com>
2025-09-10 22:46:36 -0300

    fix: remove fallback for CORS origin

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 const baseUrl = '/blog-api';
 
 const corsOptions: CorsOptions = {
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: [
