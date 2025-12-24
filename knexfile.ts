906a4ecbb93eb76732a44f01261a10dd3db3c71c
Daniel Roberto <sdanirobert@outlook.com>
2025-12-23 23:22:46 -0300

    fix: update development database connection settings in knexfile.ts

--- knexfile.ts
+++ knexfile.ts
@@ -5,10 +5,25 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.HOST || 'localhost',
-      user: process.env.POSTGRES_USER || 'user',
-      password: process.env.POSTGRES_PASSWORD || 'password',
-      database: process.env.POSTGRES_DB || 'blog_db',
+      host: 'localhost',
+      user: 'user',
+      password: 'password',
+      database: 'blog_db',
+    },
+    migrations: {
+      directory: './src/db/migrations',
+    },
+    seeds: {
+      directory: './src/db/seeds',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
     },
     migrations: {
       directory: './src/db/migrations',
